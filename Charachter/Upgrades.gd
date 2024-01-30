03f5a3ef5070257ab92b5cd3f86a4d496d861332
Jan Sambor <janeksambor@gmail.com>
2024-01-30 15:36:38 +0100

    sus

--- Charachter/Upgrades.gd
+++ Charachter/Upgrades.gd
@@ -2,5 +2,8 @@ extends Node
 
 var mower:CharacterBody2D
 
+# mower size, speed, mowing multiplier, automatic mowers flying like a dvd logo
+
+
 func scale_player(scale:Vector2):
 	mower.scale += scale
