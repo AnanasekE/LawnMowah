05063e64c132597adc49e902aa341aac5b2acd16
Jan Sambor <janeksambor@gmail.com>
2024-01-31 09:57:07 +0100

    dooot dooot

--- Charachter/Upgrades.gd
+++ Charachter/Upgrades.gd
@@ -7,3 +7,12 @@ var mower:CharacterBody2D
 
 func scale_player(scale:Vector2):
 	mower.scale += scale
+
+func speed_player(speed:float):
+	$"../Movement".SPEED += speed
+
+func add_mowing_efficiency(eff:float):
+	pass
+
+func add_mini_mower():
+	pass
